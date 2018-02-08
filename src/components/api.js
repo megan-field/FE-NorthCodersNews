9f9c795e373bd8377762d54787ce869759139034
Megan Field <megan.field@hotmail.co.uk>
2018-02-08 14:41:57 +0000

    Home page articles rendered, with voting systems in place, and each title made into a link to the single article

--- src/components/api.js
+++ src/components/api.js
@@ -5,3 +5,9 @@ export const fetchTopics = () => {
 export const fetchArticles = (topic) => {
     return topic ? fetch(`https://northcoders-news-api.herokuapp.com/api/topics/${topic}/articles`).then(res => res.json()) : fetch('https://northcoders-news-api.herokuapp.com/api/articles').then(res => res.json());
  }
+
+ export const voteArticle = (articleId, vote) => {
+    return fetch(`https://northcoders-news-api.herokuapp.com/api//articles/${articleId}?vote=${vote}`, {
+        method: 'PUT'
+    }).then(res => res.json());
+}
